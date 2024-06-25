1704a6265333a841ca71c259caaff4b03fce18be
Ilham Khalik <believerrr05@gmail.com>
2024-06-25 22:41:16 +0700

    [Feat] Make search book by title and author route controller

--- controller/getBooks.js
+++ controller/getBooks.js
@@ -2,14 +2,26 @@ const db = require('../db/connection')
 const { responseWithData, serverErrorResponse } = require('../utils/response')
 
 const getBooks = (req, res) => {
+   const { search } = req.query
    const sql = "SELECT * FROM books"
+
    db.query(sql, (err, result) => {
       if (err) {
-         serverErrorResponse(res)
-         return
+         return serverErrorResponse(res)
       }
-      
-      responseWithData(res, 200, "Success", "Get Books", result)
+
+      let books = result
+
+      if (!search) {
+         return responseWithData(res, 200, "Success", "Get Books", books)
+      }
+
+      const searchedBook = books.filter(({ title, author }) =>
+         title.toLowerCase().trim() === search.toLowerCase().trim() ||
+         author.toLowerCase().trim() === search.toLowerCase().trim()
+      )
+
+      responseWithData(res, 200, "Success", "Get Searched Book", searchedBook)
    })
 }
 
