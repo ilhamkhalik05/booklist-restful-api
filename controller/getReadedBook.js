49c22f728069e4d4b3884f7d4bc47f0b902867ec
Ilham Khalik <believerrr05@gmail.com>
2024-06-25 22:52:37 +0700

    [Bug --Fix] Solve null readed book validation issue

--- controller/getReadedBook.js
+++ controller/getReadedBook.js
@@ -5,12 +5,14 @@ const getReadedBook = (req, res) => {
    const sql = `SELECT * FROM books WHERE is_read > 0`
    db.query(sql, (err, result) => {
       if (err) {
-         serverErrorResponse(res)
-         return
+         return serverErrorResponse(res)
       }
 
-      if (result === 0) response(res, 404, 'Failed', 'You haven\'t read a single book yet!')
-      responseWithData(res, 200, 'Success', 'Get Readed Books', result)
+      if (result.length === 0) {
+         return response(res, 404, 'Failed', 'You haven\'t read a single book yet!')
+      }
+
+      return responseWithData(res, 200, 'Success', 'Get Readed Books', result)
    })
 }
 
